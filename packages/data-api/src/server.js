10c33dd65c24287023370568a49bebc8c9a19f57
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-09 00:41:33 +0200

    refactor: change mono-backend to microservices repo project struct

--- packages/data-api/src/server.js
+++ packages/data-api/src/server.js
@@ -5,31 +5,14 @@ const { json } = require("body-parser");
 const cors = require("cors");
 
 const { config } = require("./config");
-const { errorMiddleware } = require("./middlewares");
-const {
-  userRouter,
-  trackRouter,
-  playlistRouter,
-  genreRouter,
-  statsRouter,
-} = require("./routes");
+// const { errorMiddleware } = require("./middlewares");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(json());
-app.use(
-  cors({
-    origin: config.client.url,
-  }),
-);
-
-app.use(userRouter);
-app.use(trackRouter);
-app.use(playlistRouter);
-app.use(genreRouter);
-app.use(statsRouter);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send({
@@ -37,7 +20,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use(errorMiddleware);
+// app.use(errorMiddleware);
 
 module.exports = {
   app: app,
