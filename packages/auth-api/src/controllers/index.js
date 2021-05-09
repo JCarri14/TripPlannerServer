35efb8e06f623cbf88720e50b28c21c907bc828a
Jcarri14 <j.carrion@students.salle.url.edu>
2021-05-09 13:38:31 +0200

    feat: update controller indexjs

--- packages/auth-api/src/controllers/index.js
+++ packages/auth-api/src/controllers/index.js
@@ -1,5 +1,7 @@
 const userController = require("./user-controller");
+const authController = require("./auth-controller");
 
 module.exports = {
+  authController: authController,
   userController: userController,
 };
